b259c49b099c7e4694e922e700224eabe0d2dcf3
watuwa3 <watuwa3@hotmail.co.jp>
2022-06-21 20:28:10 +0900

    Chapter 3.1.1 汎用性のない Count メソッド

---
+++ ConsoleAppChapter3/Program.cs
@@ -0,0 +1,15 @@
+﻿// C#プログラミングのイディオム／定石＆パターン
+// Chapter 3.1 汎用性のない Count メソッド
+int Count(int num)
+{
+    var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+    int count = 0;
+    foreach (var number in numbers)
+    {
+        if (number == num)
+            count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Count(5) = {0}", Count(5));
