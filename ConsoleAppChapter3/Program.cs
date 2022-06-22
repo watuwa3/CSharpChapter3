96152f7b34d5f9f8c4e637f4405bc4ebe2bafc9a
watuwa3 <watuwa3@hotmail.co.jp>
2022-06-22 19:53:57 +0900

    Chapter 3.2.1 最も冗長なラムダ式 (C# 3.0以降)

--- ConsoleAppChapter3/Program.cs
+++ ConsoleAppChapter3/Program.cs
@@ -1,9 +1,19 @@
 ﻿// C#プログラミングのイディオム／定石＆パターン
 // Chapter 3.1.2 デリゲートによる実現
 // Chapter 3.1.3 匿名メソッドの利用 (C# 2.0以降)
+// Chapter 3.2.1 最も冗長なラムダ式 (C# 3.0以降)
 using System;
 using System.Numerics;
 using System.Collections.Generic;
+
+Predicate<int> judge =
+    (int n) =>
+    {
+        if (n % 2 == 0)
+            return true;
+        else
+            return false;
+    };
 
 int Count(int[] _numbers, Predicate<int> judge)
 {
@@ -19,6 +29,6 @@ int Count(int[] _numbers, Predicate<int> judge)
 
 var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
 
-var count = Count(numbers, delegate (int n) { return n % 2 != 0; });
+var count = Count(numbers, judge);
 
 Console.WriteLine("Count(n % 2) = {0}", count);
