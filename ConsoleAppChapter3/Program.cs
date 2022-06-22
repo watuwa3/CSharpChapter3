6f006ed958c671db3fb3518d4f33dfd1e4f80f8e
watuwa3 <watuwa3@hotmail.co.jp>
2022-06-22 20:04:22 +0900

    Chapter 3.2.2 judge変数の除去、if文の除去

--- ConsoleAppChapter3/Program.cs
+++ ConsoleAppChapter3/Program.cs
@@ -2,18 +2,11 @@
 // Chapter 3.1.2 デリゲートによる実現
 // Chapter 3.1.3 匿名メソッドの利用 (C# 2.0以降)
 // Chapter 3.2.1 最も冗長なラムダ式 (C# 3.0以降)
+// Chapter 3.2.2 judge変数の除去、if文の除去
+
 using System;
 using System.Numerics;
 using System.Collections.Generic;
-
-Predicate<int> judge =
-    (int n) =>
-    {
-        if (n % 2 == 0)
-            return true;
-        else
-            return false;
-    };
 
 int Count(int[] _numbers, Predicate<int> judge)
 {
@@ -29,6 +22,6 @@ int Count(int[] _numbers, Predicate<int> judge)
 
 var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
 
-var count = Count(numbers, judge);
+var count = Count(numbers, (int n) => { return n % 2 == 0; });
 
 Console.WriteLine("Count(n % 2) = {0}", count);
